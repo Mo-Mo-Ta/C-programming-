4586b701220f13f5011c78a1a13246873076a97e
Mo-Mo-Ta <mformomota200844@gmail.com>
2025-07-07 02:12:26 +0600

    Update Print.c

--- Print.c
+++ Print.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main()
 {
+  //Learning printf function 
   printf("I love myself more than anything");
   printf("\nHappier than ever");
+  printf(\"Life has taught me to be better");
   return 0;
 }
