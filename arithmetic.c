1de3814b8d0b233b429317ce6d66062c74cc18f4
Mo-Mo-Ta <mformomota200844@gmail.com>
2025-07-07 12:26:13 +0600

    Update arithmetic.c

--- arithmetic.c
+++ arithmetic.c
@@ -9,6 +9,6 @@ scanf("%d",&B);
 printf("%d + %d = %d\n",A,B,A+B);
 printf("%d - %d = %d\n",A,B,A-B);
 printf("%d * %d = %d\n",A,B,A*B);
-printf("%d / %d = %d",A,B,A/B);
+printf("%f / %d = %f",(float)A,B,(float)A/B);
 return 0;
 }
