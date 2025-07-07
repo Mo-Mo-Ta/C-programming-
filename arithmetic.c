59cbea222b4bc115b820029ff2cea239af77af14
Mo-Mo-Ta <mformomota200844@gmail.com>
2025-07-07 12:14:38 +0600

    Add files via upload

---
+++ arithmetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int A,B;
+printf("Enter a number:");
+scanf("%d",&A);
+printf("Enter a number:");
+scanf("%d",&B);
+printf("%d + %d = %d\n",A,B,A+B);
+printf("%d - %d = %d\n",A,B,A-B);
+printf("%d * %d = %d\n",A,B,A*B);
+printf("%d / %d = %d",A,B,A/B);
+return 0;
+}
